db5f67dd0c625afa5fa1ed72603302204b88ea65
Audun Halland <audun.halland@pm.me>
2023-03-08 01:34:05 +0100

    chore: More wasm experimentation

--- domain-engine-wasm/src/lib.rs
+++ domain-engine-wasm/src/lib.rs
@@ -1,4 +1,12 @@
+use ontol_compiler::{mem::Mem, Compiler};
 use wasm_bindgen::prelude::*;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
-pub struct OntolCompiler {}
+pub fn test_run_compiler() {
+    let mem = Mem::default();
+    let compiler = Compiler::new(&mem, Default::default());
+    let _env = compiler.into_env();
+}
