b515a8b9e90e1edd92d1af6aafdcb32342e766a5
Audun Halland <audun.halland@pm.me>
2023-03-29 15:34:44 +0200

    chore: Rename test file

--- ontol-compiler/tests/compiler-integration/main.rs
+++ ontol-compiler/tests/compiler-integration/main.rs
@@ -1,8 +1,8 @@
 mod test_compile_errors;
 mod test_deserialize;
 mod test_entity;
-mod test_eq_basic;
 mod test_geojson;
+mod test_map_basic;
 mod test_multi_package;
 mod test_serde;
 mod test_string_patterns;
