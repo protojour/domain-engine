51a088a5c825b4eda803c7a6bf3e41e573ecca37
Audun Halland <audun.halland@pm.me>
2023-04-02 01:36:36 +0200

    chore: Expand stix.ont for showcasing upcoming features (intersections, markdown doc)

--- ontol-compiler/tests/compiler-integration/test_stix_lite.rs
+++ ontol-compiler/tests/compiler-integration/test_stix_lite.rs
@@ -5,5 +5,17 @@ pub const STIX_ONT: &str = include_str!("../../../examples/stix_lite/stix.ont");
 
 #[test]
 fn test_stix_lite() {
-    STIX_ONT.compile_ok(|_env| {});
+    STIX_ONT.compile_fail();
+    /*
+    STIX_ONT.compile_ok(|env| {
+        let attack_pattern = TypeBinding::new(&env, "attack_pattern");
+        assert_json_io_matches!(
+            attack_pattern,
+            json!({
+                "type": "attack-pattern",
+                "created_by_ref": "identity--a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8",
+            })
+        );
+    });
+    */
 }
