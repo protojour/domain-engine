5821cd8daa00775235a92784c932612f18183fbc
Audun Halland <audun.halland@pm.me>
2023-04-03 23:51:11 +0200

    fix: 'id' for stix domain objects, note current bug

--- ontol-compiler/tests/compiler-integration/test_stix_lite.rs
+++ ontol-compiler/tests/compiler-integration/test_stix_lite.rs
@@ -17,6 +17,8 @@ fn test_stix_lite() {
             r#"missing properties, expected all of "spec_version", "created", "modified", "name" at line 1 column 25"#
         );
 
+        // BUG: In this domain at least, the ID is allocated externally.
+        // the "POST" body is currently a UNION of (id OR data), instead of an intersection
         assert_json_io_matches!(
             attack_pattern,
             json!({
