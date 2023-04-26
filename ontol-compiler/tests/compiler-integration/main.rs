738ae9ecef54f31bc38f236beeeba5c8be0f2e2d
Audun Halland <audun.halland@pm.me>
2023-04-26 20:03:45 +0200

    chore: Add conduit_public.on

--- ontol-compiler/tests/compiler-integration/main.rs
+++ ontol-compiler/tests/compiler-integration/main.rs
@@ -1,4 +1,5 @@
 mod test_compile_errors;
+mod test_conduit;
 mod test_deserialize;
 mod test_entity;
 mod test_faker;
---
+++ ontol-compiler/tests/compiler-integration/test_conduit.rs
@@ -0,0 +1,8 @@
+use ontol_test_utils::TestCompile;
+
+pub const CONDUIT_PUBLIC: &str = include_str!("../../../examples/conduit/conduit_public.on");
+
+#[test]
+fn test_compile_conduit_public() {
+    CONDUIT_PUBLIC.compile_ok(|_test_env| {});
+}
