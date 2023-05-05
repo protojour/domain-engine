931bed7f0a85a8960fc94a2e8b65a57ff7c0a1c7
Audun Halland <audun.halland@pm.me>
2023-05-06 00:54:40 +0200

    feat: More conduit experiments

--- ontol-compiler/tests/compiler-integration/test_conduit.rs
+++ ontol-compiler/tests/compiler-integration/test_conduit.rs
@@ -4,5 +4,5 @@ pub const CONDUIT_PUBLIC: &str = include_str!("../../../examples/conduit/conduit
 
 #[test]
 fn test_compile_conduit_public() {
-    CONDUIT_PUBLIC.compile_ok(|_test_env| {});
+    CONDUIT_PUBLIC.compile_fail();
 }
