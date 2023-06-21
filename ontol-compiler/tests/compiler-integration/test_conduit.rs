f999dca9556d58ab61d3f3d47cce67a2aff4430f
Audun Halland <audun.halland@pm.me>
2023-06-21 15:57:43 +0200

    test: Improve conduit_db design, some problems to solve

--- ontol-compiler/tests/compiler-integration/test_conduit.rs
+++ ontol-compiler/tests/compiler-integration/test_conduit.rs
@@ -1,8 +1,15 @@
 use ontol_test_utils::TestCompile;
+use test_log::test;
 
 pub const CONDUIT_PUBLIC: &str = include_str!("../../../examples/conduit/conduit_public.on");
+pub const CONDUIT_DB: &str = include_str!("../../../examples/conduit/conduit_db.on");
 
 #[test]
 fn test_compile_conduit_public() {
     CONDUIT_PUBLIC.compile_fail();
+}
+
+#[test]
+fn test_compile_conduit_db() {
+    CONDUIT_DB.compile_fail();
 }
