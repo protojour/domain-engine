9501c8914421b8877650a914b657f6a8f58db2b5
Audun Halland <audun.halland@pm.me>
2023-09-22 13:23:58 +0200

    fix: Json scalar accepting list input, by another juniper patch

--- domain-engine-juniper/tests/graphql-integration/test_graphql_input.rs
+++ domain-engine-juniper/tests/graphql-integration/test_graphql_input.rs
@@ -35,9 +35,11 @@ async fn test_graphql_input_deserialization_error() {
 }
 
 #[test(tokio::test)]
-// BUG in juniper
-#[should_panic = "Invalid value for argument"]
-async fn test_graphql_input_constructor_sequence() {
+// The point of this test is to pass the JSON array through juniper's
+// layer and let the input hit the data store.
+// This requires a juniper patch.
+#[should_panic = "No mock implementation found"]
+async fn test_graphql_input_constructor_sequence_as_json_scalar() {
     let (test, [schema]) = "
     def tuple {
         rel .0: i64
