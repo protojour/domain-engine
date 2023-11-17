4a471096cc636f6a9a3b556ae77c76d41c3f37dc
Audun Halland <audun.halland@pm.me>
2023-11-17 15:41:31 +0100

    chore: Remove domain-engine-wasm (moved to another repository)

--- domain-engine-wasm/src/wasm_util.rs
+++
@@ -1,5 +0,0 @@
-pub fn js_serializer() -> serde_wasm_bindgen::Serializer {
-    serde_wasm_bindgen::Serializer::new()
-        .serialize_maps_as_objects(true)
-        .serialize_missing_as_null(true)
-}
