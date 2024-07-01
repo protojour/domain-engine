83dd8e1bce22188434954cfc86920d0901dbd977
Audun Halland <audun.halland@pm.me>
2024-07-01 03:42:19 +0200

    chore: add domain-engine-store-pg and docker-compose file

---
+++ domain-engine/domain-engine-store-pg/src/lib.rs
@@ -0,0 +1,36 @@
+use deadpool_postgres::{Config, ManagerConfig, RecyclingMethod};
+
+#[allow(unused)]
+fn test_config() -> deadpool_postgres::Config {
+    let mut config = Config::new();
+    config.host = Some("localhost".to_string());
+    config.port = Some(5432);
+    config.dbname = Some("memoriam".to_string());
+    config.user = Some("memoriam".to_string());
+    config.password = Some("memoriam".to_string());
+    config.manager = Some(ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    });
+
+    config
+}
+
+#[tokio::test]
+async fn pg_test() {
+    let config = test_config();
+    let pool = config
+        .create_pool(
+            Some(deadpool_postgres::Runtime::Tokio1),
+            tokio_postgres::NoTls,
+        )
+        .unwrap();
+
+    let client = pool.get().await.unwrap();
+    let stmt = client.prepare_cached("SELECT 1 + $1").await.unwrap();
+
+    let param: i32 = 41;
+    let rows = client.query(&stmt, &[&param]).await.unwrap();
+    let value: i32 = rows[0].get(0);
+
+    assert_eq!(42, value);
+}
