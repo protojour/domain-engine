c44e692997c5770a5cbe128892e536f5933b1a34
Audun Halland <audun.halland@pm.me>
2024-08-25 03:34:59 +0200

    fix(test): add aliases back into stix graphql test (scalar array)

--- domain-engine/domain-engine-graphql/tests/graphql-integration/test_graphql_stix.rs
+++ domain-engine/domain-engine-graphql/tests/graphql-integration/test_graphql_stix.rs
@@ -59,8 +59,7 @@ async fn test_graphql_stix(ds: &str) {
                     modified: "2018-01-17T12:56:55.080Z",
                     name: "Abuse Accessibility Features",
                     description: "A malicious app could abuse Android's accessibility features to capture sensitive data or perform other malicious actions, as demonstrated in a proof of concept created by Skycure (Citation: Skycure-Accessibility).\n\nPlatforms: Android",
-                    # postgres breaks on this:
-                    # aliases: ["a", "b"],
+                    aliases: ["aliasA", "aliasB"],
                     kill_chain_phases: [
                         {
                             kill_chain_name: "mitre-mobile-attack",
