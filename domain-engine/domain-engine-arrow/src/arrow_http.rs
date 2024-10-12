3fbedec43fb4f1bcdce46341b1167e7a7ca5683c
Audun Halland <audun.halland@pm.me>
2024-10-13 01:05:10 +0200

    feat: support decoding re-chunked arrow messages

--- domain-engine/domain-engine-arrow/src/arrow_http.rs
+++ domain-engine/domain-engine-arrow/src/arrow_http.rs
@@ -1,9 +1,13 @@
 //! arrow streaming over HTTP
 
+use std::io::Read;
+
 use arrow_array::RecordBatch;
-use arrow_ipc::{reader::StreamReader, writer::StreamWriter};
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use arrow_ipc::{reader::StreamReader, writer::StreamWriter, MessageHeader};
+use bytes::{BufMut, Bytes, BytesMut};
+use bytes_deque::BytesDeque;
 use domain_engine_core::{DomainError, DomainResult};
+use flatbuffers::InvalidFlatbuffer;
 use futures_util::Stream;
 use tracing::info;
 
@@ -11,16 +15,6 @@ use crate::ArrowRespMessage;
 
 /// https://www.iana.org/assignments/media-types/application/vnd.apache.arrow.stream
 pub const MIME: &str = "application/vnd.apache.arrow.stream";
-
-#[derive(Default)]
-struct StreamEncoder {
-    arrow_stream_writer: Option<StreamWriter<bytes::buf::Writer<BytesMut>>>,
-}
-
-#[derive(Default)]
-struct StreamDecoder {
-    stream_reader: Option<StreamReader<bytes::buf::Reader<Bytes>>>,
-}
 
 /// Encode ArrowRespMessage stream for sending over HTTP chunked encoding.
 pub fn resp_msg_to_http_stream(
@@ -43,27 +37,9 @@ pub fn resp_msg_to_http_stream(
     }
 }
 
-/// Decode HTTP stream into ArrowRespMessage stream.
-/// The stream error type must be mapped to DomainError before this conversion.
-pub fn http_stream_to_resp_msg_stream(
-    http_stream: impl Stream<Item = DomainResult<Bytes>>,
-) -> impl Stream<Item = DomainResult<ArrowRespMessage>> {
-    let mut decoder = StreamDecoder::default();
-
-    async_stream::try_stream! {
-        for await bytes_result in http_stream {
-            let bytes = bytes_result?;
-            println!("received batch len={}", bytes.len());
-
-            decoder.consume_bytes(bytes)?;
-
-            println!("bytes consumed");
-
-            while let Some(batch_result) = decoder.read_batch() {
-                yield ArrowRespMessage::RecordBatch(batch_result?);
-            }
-        }
-    }
+#[derive(Default)]
+struct StreamEncoder {
+    arrow_stream_writer: Option<StreamWriter<bytes::buf::Writer<BytesMut>>>,
 }
 
 impl StreamEncoder {
@@ -95,44 +71,377 @@ impl StreamEncoder {
     }
 }
 
-impl StreamDecoder {
-    /// Feed the decoder some bytes
-    fn consume_bytes(&mut self, bytes: Bytes) -> DomainResult<()> {
-        if let Some(stream_reader) = self.stream_reader.as_mut() {
-            (*stream_reader.get_mut()) = bytes.reader();
-        } else {
-            self.stream_reader = Some(
-                StreamReader::try_new(bytes.reader(), None)
-                    .map_err(|err| DomainError::protocol(format!("{err:?}")))?,
-            );
-        }
+/// Decode HTTP stream into ArrowRespMessage stream.
+/// The stream error type must be mapped to DomainError before this conversion.
+///
+/// This is more complex because HTTP technology outside our control may
+/// re-chunk the nice original RecordBatch chunks into smaller chunks,
+/// so here we have to try to reassemble the original stream by analyzing
+/// the actual bytes in the stream to find out where each message starts and ends,
+/// or whether the stream contains a complete/incomplete messages.
+pub fn http_stream_to_resp_msg_stream(
+    http_stream: impl Stream<Item = DomainResult<Bytes>>,
+) -> impl Stream<Item = DomainResult<ArrowRespMessage>> {
+    let mut decoder = StreamDecoder::default();
 
-        Ok(())
+    async_stream::try_stream! {
+        let mut end_of_stream = false;
+
+        for await bytes_result in http_stream {
+            let bytes = bytes_result?;
+            println!("received batch len={}", bytes.len());
+
+            if !decoder.push_bytes(bytes)? {
+                // end of stream
+                break;
+            }
+
+            loop {
+                match decoder.try_decode_next_record_batch()? {
+                    ReadAttempt::Complete(batch) => {
+                        yield ArrowRespMessage::RecordBatch(batch);
+                    }
+                    ReadAttempt::Incomplete => {
+                        // wait for more bytes from http_stream
+                        break;
+                    }
+                    ReadAttempt::EndOfStream => {
+                        end_of_stream = true;
+                        break;
+                    }
+                }
+            }
+
+            if end_of_stream {
+                break;
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+struct StreamDecoder {
+    bytes_deque: bytes_deque::BytesDeque,
+    stream_reader: Option<StreamReader<bytes_deque::BytesDeque>>,
+}
+
+enum ReadAttempt<T> {
+    Complete(T),
+    Incomplete,
+    EndOfStream,
+}
+
+impl StreamDecoder {
+    fn push_bytes(&mut self, bytes: Bytes) -> DomainResult<bool> {
+        self.bytes_deque.push_back(bytes);
+
+        // try to initialize stream reader
+        if self.stream_reader.is_none() {
+            // schema must be present in the stream before stream reader can be initialized
+            match self.peek_schema()? {
+                ReadAttempt::Complete(()) => {
+                    let deque = std::mem::take(&mut self.bytes_deque);
+                    let mut stream_reader = StreamReader::try_new(deque, None)
+                        .map_err(|err| DomainError::protocol(format!("{err:?}")))?;
+                    self.bytes_deque = std::mem::take(stream_reader.get_mut());
+                    self.stream_reader = Some(stream_reader);
+
+                    Ok(true)
+                }
+                ReadAttempt::Incomplete => Ok(true),
+                ReadAttempt::EndOfStream => Ok(false),
+            }
+        } else {
+            Ok(true)
+        }
     }
 
-    /// Read the next batch
-    fn read_batch(&mut self) -> Option<DomainResult<RecordBatch>> {
+    fn try_decode_next_record_batch(&mut self) -> DomainResult<ReadAttempt<RecordBatch>> {
+        if self.stream_reader.is_none() {
+            return Ok(ReadAttempt::Incomplete);
+        }
+
+        // first do a "peek" operation to see if a complete RecordBatch is potentially ready
+        match self.peek_full_record_batch()? {
+            ReadAttempt::Complete(()) => {}
+            ReadAttempt::Incomplete => return Ok(ReadAttempt::Incomplete),
+            ReadAttempt::EndOfStream => return Ok(ReadAttempt::EndOfStream),
+        }
+
         let stream_reader = self.stream_reader.as_mut().unwrap();
 
-        println!(
-            "read batch, remaining = {}",
-            stream_reader.get_ref().get_ref().len()
-        );
+        // Swap the current bytes deque into the actual message reader
+        // so a message can be consumed for real
+        let deque = std::mem::take(&mut self.bytes_deque);
+        (*stream_reader.get_mut()) = deque;
 
-        if stream_reader.get_ref().get_ref().is_empty() {
-            return None;
+        // consume actual message
+        let next = stream_reader.next();
+
+        // swap back
+        self.bytes_deque = std::mem::take(stream_reader.get_mut());
+
+        match next {
+            Some(Ok(batch)) => Ok(ReadAttempt::Complete(batch)),
+            Some(Err(err)) => Err(DomainError::protocol(format!("{err:?}"))),
+            None => Err(DomainError::protocol("should have complete batch")),
+        }
+    }
+
+    fn peek_schema(&self) -> DomainResult<ReadAttempt<()>> {
+        let mut peekable = self.peekable_reader();
+
+        match Self::try_read_message(&mut peekable)? {
+            ReadAttempt::Complete(MessageHeader::Schema) => Ok(ReadAttempt::Complete(())),
+            ReadAttempt::Complete(_) => Err(DomainError::protocol("no schema header")),
+            ReadAttempt::Incomplete => Ok(ReadAttempt::Incomplete),
+            ReadAttempt::EndOfStream => Ok(ReadAttempt::EndOfStream),
+        }
+    }
+
+    fn peek_full_record_batch(&self) -> DomainResult<ReadAttempt<()>> {
+        let mut peekable = self.peekable_reader();
+
+        loop {
+            match Self::try_read_message(&mut peekable)? {
+                ReadAttempt::Complete(MessageHeader::RecordBatch) => {
+                    return Ok(ReadAttempt::Complete(()))
+                }
+                ReadAttempt::Complete(_) => {
+                    // any other message type is to be interpreted as an "argument" to a RecordBatch,
+                    // so we should keep looking until RecordBatch is found
+                    continue;
+                }
+                ReadAttempt::Incomplete => return Ok(ReadAttempt::Incomplete),
+                ReadAttempt::EndOfStream => return Ok(ReadAttempt::EndOfStream),
+            }
+        }
+    }
+
+    /// Get a IO reader that does not consume the actual bytes.
+    /// This is necessary for checking whether a message is complete before actully parsing it
+    fn peekable_reader(&self) -> BytesDeque {
+        // Cloning the BytesDeque is relatively cheap because Bytes::clone() is only a shallow clone
+        self.bytes_deque.clone()
+    }
+
+    /// try to consume one Arrow IPC message
+    fn try_read_message(
+        reader: &mut bytes_deque::BytesDeque,
+    ) -> DomainResult<ReadAttempt<MessageHeader>> {
+        let mut meta_size_buf: [u8; 4] = [0; 4];
+
+        if reader.read_exact(&mut meta_size_buf).is_err() {
+            return Ok(ReadAttempt::Incomplete);
         }
 
-        match stream_reader.next() {
-            Some(Ok(batch)) => Some(Ok(batch)),
-            Some(Err(err)) => {
-                println!("stream reader error: {err:?}");
-                Some(Err(DomainError::protocol(format!("{err:?}"))))
+        // all [255; 4] is a CONTINUATION_MARKER
+        if meta_size_buf == [255, 255, 255, 255] && reader.read_exact(&mut meta_size_buf).is_err() {
+            return Ok(ReadAttempt::Incomplete);
+        }
+
+        let meta_len = i32::from_le_bytes(meta_size_buf);
+
+        if meta_len == 0 {
+            return Ok(ReadAttempt::EndOfStream);
+        }
+
+        const HEADER_CHUNK_SIZE: usize = 64;
+        // repeatedly read chunks into this buffer until a msg_type can be read
+        let mut header_buf: Vec<u8> = vec![];
+
+        let msg_type = loop {
+            let prev_len = header_buf.len();
+            header_buf.extend([0; HEADER_CHUNK_SIZE]);
+
+            let chunk_len = reader
+                .read(&mut header_buf[prev_len..prev_len + HEADER_CHUNK_SIZE])
+                .map_err(|_| DomainError::protocol("could not read message header"))?;
+
+            match flatbuffers::root::<msg_type_reader::MsgTypeReader>(
+                &header_buf[0..prev_len + chunk_len],
+            ) {
+                Ok(reader) => break reader.header_type(),
+                Err(InvalidFlatbuffer::RangeOutOfBounds { .. }) => {
+                    if chunk_len < HEADER_CHUNK_SIZE {
+                        // not enough data to read the message header
+                        return Ok(ReadAttempt::Incomplete);
+                    } else {
+                        // read the exact chunk size, try to extend chunk by doing another iteration:
+                        continue;
+                    }
+                }
+                Err(err) => {
+                    info!("arrow flatbuffers error: {err:?}");
+                    return Err(DomainError::protocol("arrow IPC flatbuffers error"));
+                }
             }
-            None => {
-                println!("stream reader next returned None");
-                Some(Err(DomainError::protocol("premature end of arrow stream")))
+        };
+
+        if !reader.has_unread_capacity((meta_len - (header_buf.len() as i32)) as usize) {
+            return Ok(ReadAttempt::Incomplete);
+        }
+
+        Ok(ReadAttempt::Complete(msg_type))
+    }
+}
+
+mod msg_type_reader {
+    use arrow_ipc::{Message, MessageHeader};
+
+    /// a `flatbuffers` reader for arrow IPC messages that allows just reading the VT_HEADER_TYPE field,
+    /// so that it can work with an incomplete buffer.
+    pub struct MsgTypeReader<'a> {
+        _tab: flatbuffers::Table<'a>,
+    }
+
+    impl<'a> MsgTypeReader<'a> {
+        pub fn header_type(&self) -> MessageHeader {
+            // Safety:
+            // Created from valid Table for this object
+            // which contains a valid value in this slot
+            unsafe {
+                self._tab
+                    .get::<MessageHeader>(Message::VT_HEADER_TYPE, Some(MessageHeader::NONE))
+                    .unwrap()
             }
         }
+    }
+
+    impl flatbuffers::Verifiable for MsgTypeReader<'_> {
+        #[inline]
+        fn run_verifier(
+            v: &mut flatbuffers::Verifier,
+            pos: usize,
+        ) -> Result<(), flatbuffers::InvalidFlatbuffer> {
+            v.visit_table(pos)?
+                .visit_field::<MessageHeader>("header_type", Message::VT_HEADER_TYPE, true)?
+                .finish();
+            Ok(())
+        }
+    }
+
+    impl<'a> flatbuffers::Follow<'a> for MsgTypeReader<'a> {
+        type Inner = MsgTypeReader<'a>;
+        #[inline]
+        unsafe fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
+            Self {
+                _tab: flatbuffers::Table::new(buf, loc),
+            }
+        }
+    }
+
+    #[test]
+    fn test_read_schema() {
+        let buf: [u8; 64] = [
+            16, 0, 0, 0, 0, 0, 10, 0, 12, 0, 10, 0, 9, 0, 4, 0, 10, 0, 0, 0, 16, 0, 0, 0, 0, 1, 4,
+            0, 8, 0, 8, 0, 0, 0, 4, 0, 8, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 20, 0, 0, 0, 16, 0, 20,
+            0, 16, 0, 0, 0, 15, 0, 4, 0,
+        ];
+
+        let reader = flatbuffers::root::<MsgTypeReader>(&buf).unwrap();
+        assert_eq!(reader.header_type(), MessageHeader::Schema);
+    }
+
+    #[test]
+    fn test_read_record_batch() {
+        let buf: [u8; 64] = [
+            16, 0, 0, 0, 12, 0, 26, 0, 24, 0, 23, 0, 4, 0, 8, 0, 12, 0, 0, 0, 32, 0, 0, 0, 192, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 0, 10, 0, 20, 0, 12, 0, 8, 0, 4, 0, 10, 0,
+            0, 0, 36, 0, 0, 0, 12, 0, 0, 0,
+        ];
+
+        let reader = flatbuffers::root::<MsgTypeReader>(&buf).unwrap();
+        assert_eq!(reader.header_type(), MessageHeader::RecordBatch);
+    }
+}
+
+mod bytes_deque {
+    use std::collections::VecDeque;
+
+    use bytes::{Buf, Bytes};
+
+    /// A readable concatenation of Bytes stored in a ringbuffer
+    #[derive(Clone, Default)]
+    pub struct BytesDeque {
+        deque: VecDeque<Bytes>,
+    }
+
+    impl BytesDeque {
+        pub fn push_back(&mut self, bytes: Bytes) {
+            self.deque.push_back(bytes);
+        }
+
+        /// check whether this deque contains at least `capacity` unread bytes
+        pub fn has_unread_capacity(&self, capacity: usize) -> bool {
+            let mut cap = 0;
+
+            for bytes in &self.deque {
+                cap += bytes.remaining();
+
+                if cap >= capacity {
+                    return true;
+                }
+            }
+
+            cap >= capacity
+        }
+    }
+
+    impl std::io::Read for BytesDeque {
+        fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+            let mut read = 0;
+
+            while !buf.is_empty() {
+                let Some(bytes) = self.deque.front_mut() else {
+                    return Ok(read);
+                };
+
+                let bytes_remaining = bytes.remaining();
+
+                let len = usize::min(bytes_remaining, buf.len());
+                let mut start_reader = bytes.split_to(len).reader();
+
+                std::io::copy(&mut start_reader, &mut buf)?;
+                read += len;
+
+                if bytes.is_empty() {
+                    self.deque.pop_front();
+                }
+            }
+
+            Ok(read)
+        }
+    }
+
+    #[test]
+    fn test_bytes_deque() {
+        use std::io::Read;
+
+        let mut d = BytesDeque {
+            deque: Default::default(),
+        };
+
+        d.deque.push_back(Bytes::from_static(b"abc"));
+        d.deque.push_back(Bytes::from_static(b"def"));
+        d.deque.push_back(Bytes::from_static(b"g"));
+
+        let mut dest: [u8; 2] = [0, 0];
+
+        assert_eq!(2, d.read(&mut dest).unwrap());
+        assert_eq!(b"ab", &dest);
+        assert_eq!(3, d.deque.len());
+
+        assert_eq!(2, d.read(&mut dest).unwrap());
+        assert_eq!(b"cd", &dest);
+        assert_eq!(2, d.deque.len());
+
+        assert_eq!(2, d.read(&mut dest).unwrap());
+        assert_eq!(b"ef", &dest);
+        assert_eq!(1, d.deque.len());
+
+        assert_eq!(1, d.read(&mut dest).unwrap());
+        assert_eq!(b"gf", &dest);
+        assert_eq!(0, d.deque.len());
     }
 }
