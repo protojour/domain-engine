a174f7d091ca5fdd71b5a16173a7f86e042d240d
Eirik Krogstad <eirikkr@gmail.com>
2024-11-10 00:23:13 +0100

    feat: separate bundling for ontol-lsp desktop + web, still just one vsix.
    Improved documentation, stability, logging, added a client/server restart command.

--- ontol/ontol-language/ontol-vscode/README.md
+++ ontol/ontol-language/ontol-vscode/README.md
@@ -1,8 +1,9 @@
 # ONTOL Language Support
 
-Language server and grammar for [ONTOL](https://ontol.memoriam.net) (ONTOlogy Language), a strongly typed declarative domain model and mapping language for [Memoriam](https://memoriam.net).
+Language server and grammar for [ONTOL](https://software.situ.net/memoriam/docs/domain-engine/introduction.html) (ONTOlogy Language), a strongly typed declarative domain model and mapping language for the [Memoriam](https://software.situ.net/memoriam/docs/) domain engine.
 
-Features:
+## Features
+
 - Syntax highlighting
 - Snippets for common patterns
 - Language server, including:
@@ -11,4 +12,10 @@ Features:
   - Hover documentation on keywords, symbols and structures
   - Code completion
 
-The extension includes a Linux binary for `ontool`, providing the language server. Future versions _may_ include binaries for other platforms.
+## Dependencies
+
+The language server is compiled to WASM with a [WASI](https://github.com/WebAssembly/WASI) target, and requires the Microsoft [WASM WASI Core Extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.wasm-wasi-core), which will be installed automatically.
+
+## License
+
+MIT
--- ontol/ontol-language/ontol-vscode/src/extension.ts
+++ ontol/ontol-language/ontol-vscode/src/extension.ts
@@ -1,38 +1,72 @@
-import { ExtensionContext, Uri, window, workspace } from "vscode"
-import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node"
+import { ExtensionContext, Uri, window, workspace, commands } from "vscode"
+import { BaseLanguageClient, type LanguageClientOptions, type ServerOptions } from "vscode-languageclient"
 import { Wasm, WasmProcess, ProcessOptions } from "@vscode/wasm-wasi/v1"
 import { createStdioOptions, createUriConverters, startServer } from "@vscode/wasm-wasi-lsp"
 
-let client: LanguageClient
-let process: WasmProcess
+let client: BaseLanguageClient
+let server: WasmProcess
 
 export async function activate(context: ExtensionContext) {
+
   const channel = window.createOutputChannel("ONTOL language server", { log: true })
+  context.subscriptions.push(channel)
+
+  const decoder = new TextDecoder("utf-8")
+
+  // stderr is used all logs
+  const stderrLogger = (data: Uint8Array) => {
+    let msg = decoder.decode(data).trim()
+    if (msg.startsWith("DEBUG"))
+      channel.debug(
+        msg.replace("DEBUG", "").trim()
+      )
+    else if (msg.startsWith("WARN"))
+      channel.warn(
+        msg.replace("WARN", "").trim()
+      )
+    else if (msg.startsWith("ERROR"))
+      channel.error(
+        msg.replace("ERROR", "").trim()
+      )
+    else
+      channel.info(
+        msg.replace("INFO", "").trim()
+      )
+  }
+
+  // stdout is LSP out only
+  const stdoutLogger = (data: Uint8Array) => {
+    let msg = decoder.decode(data).trim()
+    if (msg.startsWith("Content")) return
+    channel.debug(msg)
+  }
 
   const serverOptions: ServerOptions = async () => {
     const wasm = await Wasm.load()
     const file = Uri.joinPath(context.extensionUri, "wasm", "ontol-lsp.wasm")
-    const bits = await workspace.fs.readFile(file)
-    const module = await WebAssembly.compile(bits)
+    const data = await workspace.fs.readFile(file)
+    const module = await WebAssembly.compile(data)
 
     const options: ProcessOptions = {
       stdio: createStdioOptions(),
       mountPoints: [{ kind: "workspaceFolder" }],
     }
 
-    process = await wasm.createProcess(
+    server = await wasm.createProcess(
       "ontol-lsp",
       module,
       { initial: 30, maximum: 120, shared: true },
       options,
     )
+    context.subscriptions.push({ dispose: server.terminate })
 
-    const decoder = new TextDecoder("utf-8")
-    process.stderr!.onData((data) => {
-      channel.append(decoder.decode(data) + "\n\n")
-    })
+    const stderrSub = server.stderr!.onData(stderrLogger, null)
+    const stdoutSub = server.stdout!.onData(stdoutLogger, null)
 
-    return startServer(process)
+    context.subscriptions.push(stderrSub)
+    context.subscriptions.push(stdoutSub)
+
+    return startServer(server)
   }
 
   const clientOptions: LanguageClientOptions = {
@@ -41,23 +75,38 @@ export async function activate(context: ExtensionContext) {
     uriConverters: createUriConverters(),
   }
 
-  client = new LanguageClient(
-    "ontol-language-client",
-    "ONTOL language client",
-    serverOptions,
-    clientOptions,
-    true,
-  )
+  if (process.env.PLATFORM === "node") {
+    let { LanguageClient } = await import("vscode-languageclient/node")
+    client = new LanguageClient(
+      "ontol-language-client",
+      "ONTOL language client",
+      serverOptions,
+      clientOptions,
+    )
+  } else {
+    let { LanguageClient } = await import("vscode-languageclient/browser")
+    client = new LanguageClient(
+      "ontol-language-client",
+      "ONTOL language client",
+      serverOptions,
+      clientOptions,
+    )
+  }
+
+  commands.registerCommand("ontol.restartLanguageServer", async () => {
+    await client.stop(3000)
+    await client.start()
+  })
 
   try {
     await client.start()
   } catch (error) {
-    client.error("Language service failed", error, "force")
+    client.error(`Language service failed: ${error}`, error, "force")
   }
 }
 
 export async function deactivate() {
   if (!client) return
-  await process.terminate()
+  await server.terminate()
   await client.stop()
 }
